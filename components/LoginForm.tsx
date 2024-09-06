9f10762b128cced4d7df2c5f33f24517864c8b0a
Samet Cimen <cimensamet338@gmail.com>
2024-09-05 23:44:04 -0400

    added login-form

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,15 +1,53 @@
 'use client';
 
-
+import { Form, FormField, FormLabel,FormControl, FormItem, FormDescription, FormMessage } from "@/components/ui/form";
 import { socialLogin } from "@/server/actions/auth"
 import SocialLoginForm from '@/components/SocialLoginForm'
 import { credentialLogin } from "@/server/actions/auth";
 import { useRouter } from "next/navigation";
-
+import {useForm} from 'react-hook-form'
+import {zodResolver} from '@hookform/resolvers/zod'
+import *  as z from 'zod'
+import {
+    InputOTP,
+    InputOTPGroup,
+    InputOTPSeparator,
+    InputOTPSlot,
+  } from "@/components/ui/input-otp"
+  import { Input } from "@/components/ui/input";
+import { Button } from "./ui/button";
+import { useState } from "react";
+import { FormSuccess } from "./form-sucess";
+import { FormError } from "./form-error";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
 
 export default function LoginForm(){
 
     const router = useRouter();
+    const [showTwoFactor, setShowTwoFactor] = useState(false);
+    const [error, setError] = useState("");
+    const [success, setSuccess] = useState("");
+
+
+    const LoginSchema = z.object({
+        email: z.string().email({
+            message: "Invalid Email address"
+        }),
+        password: z.string().min(1, {
+            message: "Password is required"
+        }),
+        code: z.optional(z.string())
+    })
+
+    const form = useForm<z.infer<typeof LoginSchema>>({
+        resolver: zodResolver(LoginSchema),
+        defaultValues:{
+            email:"",
+            password:"",
+            code: "",
+        }
+    });
 
     async function handleFormSubmit(e:React.FormEvent<HTMLFormElement>){
 
@@ -31,26 +69,94 @@ export default function LoginForm(){
 
     return(
         <div>
-            <form 
-                className="mt-5" 
-                onSubmit={handleFormSubmit}
-            >
-                <div>
-                    <label htmlFor="email">Email</label>
-                    <input className="bg-gray-200 ml-2 rounded " type="email" id="email" name="email" />
-                </div>
-                <div className="mt-5">
-                    <label htmlFor="password">password</label>
-                    <input className="bg-gray-200 ml-2 rounded " type="password" id="password" name="password" />
-                </div>
+                <Form {...form}>
+                    <form onSubmit={handleFormSubmit}>
+                        <div>
+                            {showTwoFactor && (
+                             <FormField
+                                control={form.control}
+                                name="code"
+                                render={({field}) => (
+                                <FormItem>
+                                    <FormLabel>Sent a two factor code to your email</FormLabel>
+                                    <FormLabel />
+                                    <FormControl>
+                                    
+                                    <InputOTP
+                                        disabled={status === "executing"}
+                                        {...field}
+                                        maxLength={6}
+                                        >
+                                        <InputOTPGroup>
+                                            <InputOTPSlot index={0} />
+                                            <InputOTPSlot index={1} />
+                                            <InputOTPSlot index={2} />
+                                            <InputOTPSlot index={3} />
+                                            <InputOTPSlot index={4} />
+                                            <InputOTPSlot index={5} />
+                                        </InputOTPGroup>
+                                        </InputOTP>
 
-                <button
-                  type="submit"
-                >
-                    Login
-                </button>
-            </form>
-            <SocialLoginForm/>
+                                    </FormControl>
+                                    <FormDescription />
+                                    <FormMessage />
+                                </FormItem>
+                                )}
+                            />
+                            )}
+
+                            {!showTwoFactor && (                            
+                                <>
+                                <FormLabel>Email</FormLabel>
+                                <FormField
+                                    control={form.control}
+                                    name="email"
+                                    render={({field}) => (
+                                    <FormItem>
+                                        <FormLabel />
+                                        <FormControl>
+                                            <Input {...field} placeholder="user@gmail.com" type='email' autoComplete="email"/>
+                                        </FormControl>
+                                        <FormDescription />
+                                        <FormMessage />
+                                    </FormItem>
+                                    )}
+                                />
+                                <FormLabel>Password</FormLabel>
+                                <FormField
+                                    control={form.control}
+                                    name="password"
+                                    render={({field}) => (
+                                    <FormItem>
+                                        <FormLabel />
+                                        <FormControl>
+                                            <Input {...field} placeholder="**********" type='password' autoComplete="current-password"/>
+                                        </FormControl>
+                                        <FormDescription />
+                                        <FormMessage />
+                                    </FormItem>
+                                    )}
+                                />
+                                </>
+                            )}
+
+                             <FormSuccess message = {success}/>
+                             <FormError message = {error}/>
+                            <Button className="px-0" size = {'sm'} variant={'link'} asChild>
+                                <Link href = "/auth/reset">
+                                    Forgot your password?
+                                </Link>
+                            </Button>
+
+                        </div>
+
+                        <Button type ='submit' className={cn('w-full my-4')}>
+                            {showTwoFactor ? "Verify" : "Sign In"}
+                        </Button>
+
+                    </form>
+                </Form>
+                <SocialLoginForm/>
         </div>
     )
 }
