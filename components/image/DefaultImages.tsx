0018b36c40107da05dc8e7375afa32bf568c7252
Samet Cimen <cimensamet338@gmail.com>
2024-10-18 00:57:45 -0400

    changed default images on app page

--- components/image/DefaultImages.tsx
+++ components/image/DefaultImages.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 import { getImageUrl } from "@/lib/utils"
 import Link from "next/link";
 export default function Page(){
-    const urls = ['34iGOQ09-SctmN7zcuk5B', 'ErJ4vNWk0ZfobHnvTnf6M', 'ejD1n-UcWatU29GFu0M-S','ejD1n-UcWatU29GFu0M-S']
+    const urls = ['WTfQL2B4IMoM_aRxeS2Ga', 'e2G8aUUn7_qPFHHp2775_', 'LwmQCakB6NPNEnX--zXZb','e4zjlxKhP3o8EsUHL5JPl']
 
     return (
         <div className="">
